58808a77e28d1867286a85114b5edb63925ad4f1
JWalters <joshwalters34@gmail.com>
2021-04-27 21:45:43 -0400

    created auth file, seeded database, started on some routes and views

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,10 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+// const User = require('./userData');
+// const Post = require('./postData')
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
+// const postData = require('./postData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +13,12 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  // await Post.bulkCreate(postData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // })
+   
 
   process.exit(0);
 };

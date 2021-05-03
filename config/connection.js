2ec75fb8336cc9698c5c0f7c83e35c6dbd0a307f
JWalters <joshwalters34@gmail.com>
2021-05-02 21:10:02 -0400

    updated config file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL); {
+    define: {timestamps: false}
+  }
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
